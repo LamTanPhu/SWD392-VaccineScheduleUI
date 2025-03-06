0e8a5d59a6f1186078039679d7ae0cb546c0aa17
Minh Khang <khangng3012@gmail.com>
2025-03-06 15:47:14 +0700

    HomePage, Header, Footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from "react-router";
+import Header from './components/common/header.jsx';
+import Footer from './components/common/footer.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header/>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} /> 
+      </Routes>
+    </BrowserRouter>
+    <Footer />
   </StrictMode>,
 )
