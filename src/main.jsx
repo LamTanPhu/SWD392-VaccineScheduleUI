bd99c36ff1723d4549f97561b727036456c32aad
Minh Khang <khangng3012@gmail.com>
2025-03-06 23:19:49 +0700

    About page

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from "react-router";
 import Header from './components/common/header.jsx';
 import Footer from './components/common/footer.jsx';
+import About from './pages/about.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route index element={<App />} /> 
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
     <Footer />
